cd0120374644a50ec8a47ff5c078daedd3bbe617
Liviiasolo <lidoviktoria@gmail.com>
2026-07-13 22:40:04 +0200

    change script

--- js/script.js
+++ js/script.js
@@ -54,35 +54,34 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
 
-// Slider
+// // Slider
 
 document.addEventListener("DOMContentLoaded", () => {
-
   let slideToShow = 0;
 
   const modalBtns = document.querySelectorAll('.gallery-modal-btn');
   const sliderContainer = document.querySelector('.splide');
 
-  const slider = new Splide(sliderContainer).mount();
+  if (sliderContainer) {
+    const slider = new Splide(sliderContainer).mount();
 
-  // Логіка вибору слайду при кліку
-  modalBtns.forEach(function (button) {
-    button.addEventListener('click', function () {
-      const slideNumberToShow = button.getAttribute('data-index');
-      slideToShow = Number(slideNumberToShow);
-      console.log("Обраний слайд:", slideToShow);
+    // Логіка вибору слайду при кліку
+    modalBtns.forEach(function (button) {
+      button.addEventListener('click', function () {
+        const slideNumberToShow = button.getAttribute('data-index');
+        slideToShow = Number(slideNumberToShow);
+        console.log("Обраний слайд:", slideToShow);
+      });
     });
-  });
 
-  // Запуск MicroModal після того, як визначено `slider`
-  MicroModal.init({
-    onShow: function () {
-      slider.go(slideToShow);
-    }
-  });
-
+    // Запуск MicroModal після того, як визначено `slider`
+    MicroModal.init({
+      onShow: function () {
+        slider.go(slideToShow);
+      }
+    });
+  }
 });
-
 
 
 // Гамбургер-меню
